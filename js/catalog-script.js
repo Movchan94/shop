96bf65bb7a0ff163c1448c31cde44e1af29b1cb2
Movchan94 <tkd.movchan@gmail.com>
2020-09-06 22:48:19 +0300

    Added drop-down list and input rage price.

--- js/catalog-script.js
+++
@@ -1,8 +0,0 @@
-
-let slider = document.getElementById("myRange");
-let output = document.getElementById("demo");
-output.innerHTML = slider.value;
-
-slider.oninput = function() {
-        output.innerHTML = this.value;
-    }
