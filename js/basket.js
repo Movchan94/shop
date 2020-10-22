741ab7c78c79154b4a2edfa0e9051947b9338a3a
unknown <you@example.com>
2020-10-22 17:16:30 +0300

    fixed page basket

---
+++ js/basket.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+    // Находим плавающий блок и делаем его плавающим
+    $('.price-card').stick_in_parent({
+    // Отступ сверху
+    offset_top: 350
+    });
+    });
+
+
